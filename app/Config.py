308f24c46a0cf3384b45c56df7e8c43d29be458a
Max Saber <maxsaber@users.noreply.github.com>
2023-04-14 18:55:59 -0400

    Update Config.py

--- app/Config.py
+++ app/Config.py
@@ -1,6 +1,6 @@
 config = {
     "learn_rest_key" : "6d18d845-ca50-4eb3-944e-d532af552ad0",
-    "learn_rest_secret" : "ZXXX3kSLFcFFtBdU5NXfEs2mJhtqPiMr",
+    "learn_rest_secret" : "",
     "learn_rest_url" : "mcphssaas-test.blackboard.com",
     "app_url" : "https://bbdn-quick-enroll-production.up.railway.app",
     "verify_certs" : "True"
